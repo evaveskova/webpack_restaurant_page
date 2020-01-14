65c223e06ca1971caedc4b925693d62938a87b0a
evaveskova <evaveskova@gmail.com>
2020-01-14 15:31:36 +0100

    append description p tag in index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ src.appendChild(img);
 let header = document.createElement("h1");
 header.textContent = "The World's Greatest Restaurant";
 document.getElementById("content").appendChild(header);
+
+let description = document.createElement("p");
+description.textContent = `Do you like food? Great! Because we have lots of it.
+That's because we're a restaurant, and restaurants serve food.
+We've been around since 1202, starting out as a small pub that serves
+sheep's intenstines, to a renowed, 5-star establishment with
+delightful, overpriced, tiny food. Enjoy!`;
+document.getElementById("content").appendChild(description);
