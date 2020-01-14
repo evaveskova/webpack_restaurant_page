0beca6762a9c7071e01c5628c434c5d949c2371d
evaveskova <evaveskova@gmail.com>
2020-01-14 15:16:14 +0100

    append image in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('nesto');
+let img = document.createElement("img");
+img.src = "../src/assets/food_plate.jpg";
+var src = document.getElementById("content");
+src.appendChild(img);
+console.log("an image appeared!");
