3c7cd30ba8ac46d9165960c459540b3a7c7cef54
evaveskova <evaveskova@gmail.com>
2020-01-14 17:11:17 +0100

    add home page module and exported to index.js

---
+++ src/home.js
@@ -0,0 +1,22 @@
+import "./style.css"
+
+function homePage() {
+  let img = document.createElement("img");
+  img.src = "../src/assets/food_plate.jpg";
+  let src = document.getElementById("content");
+  src.appendChild(img);
+
+  let description = document.createElement("p");
+  description.textContent = `Do you like food? Great! Because we have lots of it.
+  That's because we're a restaurant, and restaurants serve food.
+  We've been around since 1202, starting out as a small pub that serves
+  sheep's intenstines, to a renowed, 5-star establishment with
+  delightful, overpriced, tiny food. Enjoy!`;
+  document.getElementById("content").appendChild(description);
+
+  console.log("home");
+};
+
+
+
+export default homePage;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import "./style.css"
+import homePage from "./home.js"
 
 let header = document.createElement("h1");
 header.textContent = "The World's Greatest Restaurant";
 document.getElementById("content").appendChild(header);
-
-let img = document.createElement("img");
-img.src = "../src/assets/food_plate.jpg";
-let src = document.getElementById("content");
-src.appendChild(img);
-
-let description = document.createElement("p");
-description.textContent = `Do you like food? Great! Because we have lots of it.
-That's because we're a restaurant, and restaurants serve food.
-We've been around since 1202, starting out as a small pub that serves
-sheep's intenstines, to a renowed, 5-star establishment with
-delightful, overpriced, tiny food. Enjoy!`;
-document.getElementById("content").appendChild(description);
+debugger;
+homePage();
